023085ef264bbb2747ba05d4566e2604e89292f5
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-09 13:40:52 +0800

    Complete all tests for sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if(number1 < 0 || number2 < 0 || typeof(number1) != 'number' || typeof(number2) != 'number') {
+        return'ERROR'
+    }
+    else if(number1 > number2){
+        for(let i = number2+1; i!= number1+1; i++){
+            number2+=i;
+        }
+        return number2;
+    }
+    else{
+        for(let i = number1+1; i!= number2+1; i++){
+            number1+=i;
+        }
+        return number1;
+    }
+    
 
 };
 
