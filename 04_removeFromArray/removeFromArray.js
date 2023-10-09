ab1115f89192b769ea9374d5aa40b7b967056cc6
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-09 11:56:55 +0800

    Complete 1st test from removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, elementToRemove) {
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === elementToRemove){
+            array.splice(i, 1);
+        }
+    }
+    return array;
+    
 };
 
 // Do not edit below this line
