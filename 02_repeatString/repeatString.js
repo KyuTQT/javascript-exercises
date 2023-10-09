0fe5526ee5e55337988026b66a629f38e0aee36b
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-09 11:31:04 +0800

    Completre repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(phraseToRepeat, numberOfTimesToRepeat) {
+    let phraseToAdd = phraseToRepeat;
 
+    if(numberOfTimesToRepeat === 0){
+        return '';
+    }
+    else if(numberOfTimesToRepeat < 0) {
+        return 'ERROR';
+    }
+    else{
+        for(let i = 1; i != numberOfTimesToRepeat; i++){
+            phraseToRepeat+= phraseToAdd;
+        }
+        return phraseToRepeat;
+    }
+
+    
+    
 };
 
 // Do not edit below this line
