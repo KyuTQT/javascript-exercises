2b41e73015535684a4bded65b20c9b962ac4adee
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-09 14:04:23 +0800

    Complete farenheit to celsius tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  farenheit = (farenheit - 32) * (5/9);
+  farenheit = Math.round(farenheit * 10)/10;
+  return farenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
 };
 
 // Do not edit below this line
