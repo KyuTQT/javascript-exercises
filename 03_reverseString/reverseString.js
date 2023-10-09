a39152c2fd2c245181a37c50dfa33feb279109a4
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-09 11:39:53 +0800

    Complete 1st test reverseTest

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let output = '';
+    for(let i = 1; i != stringToReverse.length+1; i++){
+        output += stringToReverse[stringToReverse.length-i];
+    }
+    return output;
 };
 
 // Do not edit below this line
