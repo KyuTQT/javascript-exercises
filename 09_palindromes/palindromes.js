7fdcbbf66df1a63ae6c2c234c7220951b255c5e5
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-17 11:17:10 +0800

    Complete palindrome exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text
+                .replace(/[^\w\s]/gi, '')
+                .replace(/\s/g, '')
+                .toLowerCase();
+    cleanedStr = text
+                    .split('')
+                    .reverse()
+                    .join('')
+                    .replace(/[^\w\s]/gi, '')
+                    .replace(/\s/g, '')
+                    .toLowerCase();
+    console.log(text);
+    console.log(cleanedStr);
+    return text === cleanedStr ? true : false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
