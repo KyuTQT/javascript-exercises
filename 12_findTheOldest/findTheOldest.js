513935048c079600e5239175871dc21ea205975f
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-17 13:04:47 +0800

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    yearNow = new Date().getFullYear();
+    let sorted = people.sort((currentPerson, nextPerson) => {
+        let currentAge = currentPerson.yearOfDeath 
+        ? currentPerson.yearOfDeath - currentPerson.yearOfBirth 
+        : yearNow - currentPerson.yearOfBirth;
 
+        let nextAge = nextPerson.yearOfDeath 
+        ? nextPerson.yearOfDeath - nextPerson.yearOfBirth 
+        : yearNow - nextPerson.yearOfBirth;
+
+        return currentAge > nextAge ? -1 : 1;
+    })
+    return sorted[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
