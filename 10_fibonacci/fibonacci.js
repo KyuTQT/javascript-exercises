fe7900bceb7beae661b1c6c6633aa2d19fd68766
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-17 11:17:19 +0800

    Complete fibonacci exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    if(typeof value === 'String'){
+       value = value.parseInt(value);
+    }
+    
+    if(value < 0){
+        return 'OOPS';
+    }
 
+    sum = 0;
+    fibonacciArray = [1, 1];
+    for(let i = 1; i < value-1; i++){
+        sum = fibonacciArray[i]+ fibonacciArray[i-1];
+        fibonacciArray.push(sum);
+        sum = 0;
+    }
+    return fibonacciArray[fibonacciArray.length-1];
 };
 
 // Do not edit below this line
