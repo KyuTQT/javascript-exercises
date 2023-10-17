ab195e445effb63fd30d88812360e473de293c31
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-17 10:40:11 +0800

    Complete all arithmetic exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(...values) {
+  let sum = 0;
+  for (let index = 0; index < values.length; index++) {
+    sum  += values[index];
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+  let sum = 0;
+	if(values.length.length === 0){
+    return sum;
+  }
+  
+  for(let i = 0; i < values.length; i++){
+    sum += values[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(values) {
+  product = values[0];
+  for(let i = 1; i<values.length; i++){
+    product *= values[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(value, raised) {//3
+  let powered = value;
+	for(let i = 2; i <= raised; i++){
+    powered *= value;
+    console.log (value);
+  }
+  return powered;
 };
 
-const factorial = function() {
+const factorial = function(value) {
+  if (value === 0 || value === 1) return 1;
 	
+  factored = value
+  for(let i = value-1; i != 0; i--){
+    factored *= i
+  }
+  return factored;
 };
 
 // Do not edit below this line
